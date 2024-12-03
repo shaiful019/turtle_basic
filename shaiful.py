388f00457c5daa6e9b02837ab04d96f111d3b867
Shaiful Islam <shaiful019@gmail.com>
2024-12-03 20:05:51 +0600

    first push

---
+++ shaiful.py
@@ -0,0 +1,23 @@
+import turtle
+
+t = turtle.Turtle()
+t.speed(0)
+
+t.penup()
+t.goto(-200, 0)
+t.pendown()
+
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+
+turtle.done()
